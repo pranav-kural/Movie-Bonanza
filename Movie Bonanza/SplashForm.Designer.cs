434a8cbad2756a94e57495fc43beead6526b2dfb
Pranav Kural <pranavkural042@gmail.com>
2017-02-27 16:35:25 -0500

    Added the splash form

---
+++ Movie Bonanza/SplashForm.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace Movie_Bonanza
+{
+    partial class SplashForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MovieBonanzoLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // MovieBonanzoLabel
+            // 
+            this.MovieBonanzoLabel.AutoSize = true;
+            this.MovieBonanzoLabel.Font = new System.Drawing.Font("Century Gothic", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MovieBonanzoLabel.ForeColor = System.Drawing.Color.DarkSalmon;
+            this.MovieBonanzoLabel.Location = new System.Drawing.Point(222, 3);
+            this.MovieBonanzoLabel.Name = "MovieBonanzoLabel";
+            this.MovieBonanzoLabel.Size = new System.Drawing.Size(383, 58);
+            this.MovieBonanzoLabel.TabIndex = 0;
+            this.MovieBonanzoLabel.Text = "Movie Bonanzo";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Image = global::Movie_Bonanza.Properties.Resources.movie_bonanza_logo;
+            this.pictureBox1.Location = new System.Drawing.Point(155, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(476, 453);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // SplashForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Brown;
+            this.ClientSize = new System.Drawing.Size(795, 469);
+            this.Controls.Add(this.MovieBonanzoLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "SplashForm";
+            this.Text = "SplashForm";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label MovieBonanzoLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
